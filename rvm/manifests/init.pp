cf7cb7918183fc9e2b2ef4dd4624ec42dacecb57
Juan Manuel Barreneche <snipperme@gmail.com>
2011-09-29 19:42:02 -0300

    Add rvm::user definition to allow users configuration

--- rvm/manifests/init.pp
+++ rvm/manifests/init.pp
@@ -1,20 +1,32 @@
 class rvm {
-  class rvm::dependencies {
+
+  class dependencies {
     package {
       "build-essential": ensure => installed;
-      "curl": ensure => installed;
+      "curl":     ensure => installed;
       "git-core": ensure => installed;
     }
   }
-  include rvm::dependencies
+
+  define user ( $user = 'root' ) {
+    exec {'group-assign':
+      unless  => "/usr/bin/groups ${user} | grep rvm",
+      command => "/usr/sbin/usermod -aG rvm ${user}",
+    }
+  }
+
+  include dependencies
+  user {'rvm::user::root': user => 'root', }
+
   exec { "rvm-install":
     alias   => 'rvm',
-    user    => 'root',
     cwd     => "/tmp",
     path    => '/usr/bin:/usr/sbin:/bin',
     unless  => 'which rvm',
     command => "bash -c '/usr/bin/curl -s https://rvm.beginrescueend.com/install/rvm -o rvm-installer ; chmod +x rvm-installer ; rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man sudo ./rvm-installer'",
     require => Class['rvm::dependencies'], 
   }
+
   Class["rvm"] -> Package<| provider == rvm |>
+
 }
--- rvm/test/manifests/base.pp
+++ rvm/test/manifests/base.pp
@@ -9,6 +9,16 @@ package {
 
 include rvm
 
+user {deploy:
+  ensure     => present,
+  shell      => '/bin/bash',
+  home       => '/home/deploy',
+  managehome => true,
+  require    => [Group[puppet]],
+}
+
+rvm::user {'rvm::user::deploy': user => 'deploy', }
+
 package {"ruby-1.9.2-p290":
   provider => rvm,
 }
