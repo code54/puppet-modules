f875bd46e9293f46558b2f4b80e86371d8397e36
Juan Manuel Barreneche <snipperme@gmail.com>
2011-10-05 13:14:22 -0300

    Change passenger apache installation to log only on_failure

--- rvm/manifests/passenger/apache.pp
+++ rvm/manifests/passenger/apache.pp
@@ -12,7 +12,7 @@ class rvm::passenger::apache(
     path      => "/usr/local/rvm/bin:/usr/bin:/usr/sbin:/bin",
     # Cut the path for the .so file from the snippet (1st row 3rd field)
     unless    => "test -f \"`rvm $ruby exec passenger-install-apache2-module  --snippet | head -n 1 | cut -f 3 -d ' '`\"",
-    logoutput => true,
+    logoutput => on_failure,
     require   => Package["$ruby@global:passenger"],
   }
 }
