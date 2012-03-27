e24d6635bfcd68d26df2e04d38ef8beb85e12302
Juan Manuel Barreneche <snipperme@gmail.com>
2012-03-27 11:14:13 -0300

    Fix puppet warning (scope variable)

--- rvm/manifests/init.pp
+++ rvm/manifests/init.pp
@@ -1,7 +1,7 @@
 class rvm {
 
   class dependencies {
-    case $operatingsystem {
+    case $::operatingsystem {
       'centos', 'redhat', 'fedora': {
         $dependencies_names = ['gcc-c++', 'patch', 'readline', 'readline-devel', 'zlib', 'zlib-devel', 'libyaml-devel',
         'libffi-devel', 'openssl-devel', 'make', 'bzip2', 'autoconf', 'automake', 'libtool', 'bison', 'file']
